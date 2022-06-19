9549b0432cd1cbd5306634151dbefb34d520a180
TimonLukas <github@timonlukas.de>
2022-06-19 01:02:27 +0000

    Deploying to gh-pages from @ TimonLukas/timonlukas.github.io@22cece1a41edf6465593cb7646adf03af987130a 🚀

---
+++ assets/Background.entry.bf2c5c91.js
@@ -0,0 +1,1 @@
+import{c as e}from"./entry.7e323ff6.js";import{bl as o}from"./index.23d3cad9.js";const n=e({component:o,controls:{blobAmount:{label:"Number of blobs",type:"range",min:1,max:20,initial:1},velocityCoefficient:{label:"Velocity coefficient",type:"range",min:0,max:10,step:.01,initial:1}}});export{n as entry};
